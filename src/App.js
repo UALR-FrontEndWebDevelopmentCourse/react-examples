7da161ae1fa5f6f2fb400563ef5d2b7959b1a08a
irconde <irconde@ualr.edu>
2023-03-05 14:59:52 -0600

    Avatar component created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import './App.css';
-import Gallery, {Profile} from "./Gallery";
+import Avatar from "./Avatar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <Gallery/>
-          <Profile/>
+          <div>
+              <Avatar
+                  size={100}
+                  person={{
+                      name: 'Katsuko Saruhashi',
+                      imageId: 'YfeOqp2'
+                  }}
+              />
+              <Avatar
+                  size={80}
+                  person={{
+                      name: 'Aklilu Lemma',
+                      imageId: 'OKS67lh'
+                  }}
+              />
+              <Avatar
+                  size={50}
+                  person={{
+                      name: 'Lin Lanying',
+                      imageId: '1bX5QH6'
+                  }}
+              />
+          </div>
       </header>
     </div>
   );
