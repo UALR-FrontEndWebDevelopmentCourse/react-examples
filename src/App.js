55f857aac7d59559de2dd20a387d93fb994f6702
irconde <irconde@ualr.edu>
2023-03-05 11:19:04 -0600

    Created and used several times a Welcome component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>;
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Hello World.
-        </p>
+          <Welcome name="Ivan Rodriguez-Conde" />
+          <Welcome name="Tinker Hatfield" />
+          <Welcome name="Dieter Rams" />
+          <Welcome name="Shawn Carter" />
       </header>
     </div>
   );
