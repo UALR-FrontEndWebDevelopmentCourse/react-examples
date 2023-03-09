e8c58adb7a0a3f834cbc360f94de571f99e78adf
irconde <irconde@ualr.edu>
2023-03-09 09:16:25 -0600

    New List component created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
 import './App.css';
-import Item from "./Components/Item";
+import List from "./Components/List";
 
 function App() {
     return (
-        <section>
-            <h1>Sally Ride's Packing List</h1>
-            <ul>
-                <Item
-                    isPacked={true}
-                    name="Space suit"
-                />
-                <Item
-                    isPacked={true}
-                    name="Helmet with a golden leaf"
-                />
-                <Item
-                    isPacked={false}
-                    name="Photo of Tam"
-                />
-            </ul>
-        </section>
+        <section><List/></section>
     );
 }
 
