4e1731cf576082dcd8526bd0768deadc814ecfbf
irconde <irconde@ualr.edu>
2023-03-09 22:54:22 -0600

    Multiple state variables

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import './App.css';
-import Toolbar from "./Components/Toolbar";
+import { useState } from 'react';
+import { sculptureList } from './data.js';
 
-function App() {
+export default function App() {
+    const [index, setIndex] = useState(0);
+    const [showMore, setShowMore] = useState(false);
+
+    function handleNextClick() {
+        setIndex(index + 1);
+    }
+
+    function handleMoreClick() {
+        setShowMore(!showMore);
+    }
+
+    let sculpture = sculptureList[index];
     return (
-        <Toolbar/>
+        <>
+            <button onClick={handleNextClick}>
+                Next
+            </button>
+            <h2>
+                <i>{sculpture.name} </i>
+                by {sculpture.artist}
+            </h2>
+            <h3>
+                ({index + 1} of {sculptureList.length})
+            </h3>
+            <button onClick={handleMoreClick}>
+                {showMore ? 'Hide' : 'Show'} details
+            </button>
+            {showMore && <p>{sculpture.description}</p>}
+            <img
+                src={sculpture.url}
+                alt={sculpture.alt}
+            />
+        </>
     );
 }
-
-export default App;
