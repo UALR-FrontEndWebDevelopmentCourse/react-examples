77e10edbdbc75a68bbda623d588860568576aefb
irconde <irconde@ualr.edu>
2023-03-19 23:31:23 -0500

    Navigation based on react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import List from "./Components/InsertList";
+import Header from "./Components/Header";
+import Main from "./Components/Main";
 
+// TODO 02. Here we define the routes
+/*
+Our application is defined within the <App> component.
+To simplify things, we will split our application into two parts.
+The <Header> component will contain links to navigate throughout the website.
+The <Main> component is where the rest of the content will be rendered.
+ */
 export default function App() {
     return (
-        <List/>
+        <div>
+            <Header/>
+            <Main/>
+        </div>
     );
 }
