fc5f74aa3bc87161ace5ae154ac305c940b14e6d
irconde <irconde@ualr.edu>
2023-03-20 15:53:49 -0500

    MockForm component created

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import NameForm from "./Components/NameForm";
-import UserForm from "./Components/UserForm";
-import UncontrolledNameForm from "./Components/UncontrolledNameForm";
-import UncontrolledRefNameForm from "./Components/UncontrolledRefNameForm";
+import MockForm from "./Components/MockForm";
 
 export default function App() {
     return (
         <div>
-            <h1>Uncontrolled components</h1>
-            <h2>Function-based components</h2>
-            <UncontrolledNameForm/>
-            <UncontrolledRefNameForm/>
+            <MockForm status={"error"}/>
         </div>
     );
 }
