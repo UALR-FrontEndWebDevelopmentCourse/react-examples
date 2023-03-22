49948306b2a7eab8e06788a8da1343c78aaad26f
irconde <irconde@ualr.edu>
2023-03-21 19:10:43 -0500

    Creating a redux architecture based on redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,47 +1,7 @@
-import {useReducer} from 'react';
-import AddTask from './Components/AddTask.js';
-import TaskList from './Components/TaskList.js';
-import tasksReducer from "./Components/taskReducer";
+import {Counter} from "./Components/Counter";
 
-export default function TaskApp() {
-    const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
-    function handleAddTask(text) {
-        dispatch({
-            type: 'added',
-            id: nextId++,
-            text: text,
-        });
-    }
-
-    function handleChangeTask(task) {
-        dispatch({
-            type: 'changed',
-            task: task,
-        });
-    }
-
-    function handleDeleteTask(taskId) {
-        dispatch({
-            type: 'deleted',
-            id: taskId,
-        });
-    }
+export default function App() {
     return (
-        <>
-            <h1>Prague itinerary</h1>
-            <AddTask onAddTask={handleAddTask} />
-            <TaskList
-                tasks={tasks}
-                onChangeTask={handleChangeTask}
-                onDeleteTask={handleDeleteTask}
-            />
-        </>
+        <Counter/>
     );
 }
-
-let nextId = 3;
-const initialTasks = [
-    {id: 0, text: 'Visit Kafka Museum', done: true},
-    {id: 1, text: 'Watch a puppet show', done: false},
-    {id: 2, text: 'Lennon Wall pic', done: false},
-];
--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from "./couterSlice";
 
 export default configureStore({
-    reducer: {},
+    reducer: {
+        counter: counterReducer,
+    },
 })
